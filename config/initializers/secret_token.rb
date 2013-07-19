ceb3fac5825327dcd56aeeeb79060f94d79d1b88
Alvaro A <alv.ale.alvarez@gmail.com>
2013-07-19 01:42:35 -0400

    Commit Inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Llenemoslo::Application.config.secret_key_base = 'f21173a2f5483a67c67581796fb12abacdc71ce2d8d430172f13165ee5c2c2e6baaf86bf22b9161d45e5854db6df839a054a58c7e28898fd6944c84a927761cf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Llenemoslo::Application.config.session_store :cookie_store, key: '_llenemoslo_session'
