ecf688512defa906e60f6eb35b95df8da088fc40
Alvaro Alvarez <alv.ale.alvarez@gmail.com>
2013-10-05 14:40:16 -0300

    Eliminando logica de controlador, sustituida por panel de administracion

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   load_and_authorize_resource
-  before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :set_event, only: [:show]
   
   def index
     @events = Event.all
@@ -9,43 +9,9 @@ class EventsController < ApplicationController
   def show
   end
 
-  def new
-    @event = Event.new
-  end
-
-  def create
-    @event = Event.new(event_params)
-    if @event.save
-      redirect_to @event
-    else
-      render "new"
-    end
-  end
-
-  def edit
-  end
-
-  def update
-    if @event.update(event_params)
-      redirect_to event_path
-    else
-      render "edit"
-    end
-  end
-
-  def destroy
-    @event.destroy
-    redirect_to @event  
-  end
-  
-
-  private
+   private
 
     def set_event
       @event = Event.find(params[:id])
     end
-
-    def event_params
-      params.require(:event).permit(:artista, :fecha_evento, :lugar ,:descripcion, :fecha_cierre, :imagen)
-    end  
 end
